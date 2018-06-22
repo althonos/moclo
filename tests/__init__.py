4a86e3a4e80dcb9740ea29693508cd2130444f24
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-06-22 13:57:01 +0200

    Always run tests against the local codebase

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,24 +2,17 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-try:
-    # Try to load the system version
-    import moclo.kits.ytk
+import importlib
+import os
+import sys
 
-except ImportError:
+# Patch the PYTHONPATH to find the base moclo package
+proj = os.path.abspath(os.path.join(__file__, '..', '..'))
+sys.path.insert(0, os.path.join(proj, 'moclo'))
 
-    import importlib
-    import os
-    import sys
+# Load the kits namespace and add additional plugins packages
+import moclo.kits
 
-    # Patch the PYTHONPATH to find the base moclo package
-    proj = os.path.abspath(os.path.join(__file__, '..', '..'))
-    sys.path.insert(0, os.path.join(proj, 'moclo'))
-    importlib.reload(sys.modules['moclo'])
-
-    # Load the kits namespace and add additional plugins packages
-    import moclo.kits
-
-    # Yeast ToolKit
-    moclo.kits.__path__.append(os.path.join(proj, 'moclo-ytk', 'moclo', 'kits'))
-    import moclo.kits.ytk
+# Yeast ToolKit
+moclo.kits.__path__.append(os.path.join(proj, 'moclo-ytk', 'moclo', 'kits'))
+import moclo.kits.ytk
