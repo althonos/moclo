10434e74187d52862e0f1bd3a9ce4cade09d8c3d
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-07-05 11:47:29 +0200

    Add more tests for `moclo.record`

--- tests/test_record.py
+++ tests/test_record.py
@@ -6,6 +6,7 @@ import unittest
 
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation, ExactPosition
 
 from moclo.record import CircularRecord
 
@@ -23,6 +24,44 @@ class TestCircularRecord(unittest.TestCase):
         self.assertEqual(cr.seq, sr.seq)
         self.assertEqual(cr.id, sr.id)
 
+    def test_linear_error(self):
+        """Assert a `CircularRecord` does not accept a linear record.
+        """
+        an = {'topology': 'linear'}
+        sr = SeqRecord(seq=Seq('ATGC'), id='linear', annotations=an)
+        self.assertRaises(ValueError, CircularRecord, sr)
+
+    def test_contains(self):
+        """Assert `_ in CircularRecord` works as expected.
+        """
+        sr = SeqRecord(seq=Seq("ATGC"), id="test_init")
+        cr = CircularRecord(sr)
+        self.assertIn('ATGC', cr)
+        self.assertIn('GCAT', cr)
+        self.assertNotIn('ATGCAT', cr)
+
+    def test_shift_features(self):
+        """Assert a `CircularRecord` shifts its features as intended.
+        """
+        ft = [
+            SeqFeature(FeatureLocation(ExactPosition(0), ExactPosition(2), strand=+1), type='promoter'),
+            SeqFeature(FeatureLocation(ExactPosition(2), ExactPosition(4), strand=+1), type='promoter')
+        ]
+        sr = SeqRecord(seq=Seq('ATGC'), id='feats', features=ft)
+        cr = CircularRecord(sr)
+
+        cr_1 = cr >> 1
+        self.assertEqual(cr_1.features[0].location, FeatureLocation(ExactPosition(1), ExactPosition(3), strand=+1))
+        self.assertEqual(cr_1.features[1].location, FeatureLocation(ExactPosition(3), ExactPosition(5), strand=+1))
+
+        cr_2 = cr_1 >> 1
+        self.assertEqual(cr_2.features[0].location, FeatureLocation(ExactPosition(2), ExactPosition(4), strand=+1))
+        self.assertEqual(cr_2.features[1].location, FeatureLocation(ExactPosition(0), ExactPosition(2), strand=+1))
+
+        cr_3 = cr_2 >> 1
+        self.assertEqual(cr_3.features[0].location, FeatureLocation(ExactPosition(3), ExactPosition(5), strand=+1))
+        self.assertEqual(cr_3.features[1].location, FeatureLocation(ExactPosition(1), ExactPosition(3), strand=+1))
+
     def test_shift_seq(self):
         """Assert a `CircularRecord` shifts its sequence as intended.
         """
@@ -39,6 +78,9 @@ class TestCircularRecord(unittest.TestCase):
         self.assertEqual((cr << 0).seq, cr.seq)
         self.assertEqual((cr << len(cr)).seq, cr.seq)
         self.assertEqual((cr << -5).seq, (cr >> 5).seq)
+
+        self.assertEqual((cr << -3).seq, (cr >> 3).seq)
+        self.assertEqual((cr >> -3).seq, (cr << 3).seq)
 
     def test_add(self):
         """Assert adding to a `CircularRecord` raises a type error.
