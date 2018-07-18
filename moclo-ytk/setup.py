fbf64919bd9f9ce373757aa49ac37ee97e828751
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-07-18 14:21:04 +0200

    Use version-specific `bz2` textual opener to `setup.py`

--- moclo-ytk/setup.py
+++ moclo-ytk/setup.py
@@ -2,22 +2,30 @@
 # coding: utf-8
 
 import bz2
-# import distutils.cmd
-import distutils.core
 import json
 import glob
 import os
 import setuptools
+import sys
 
+from distutils.extension import Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 
 
-class Registry(distutils.core.Extension):
+if sys.version_info[0] == 2:
+    def bz2_open_write(filename):
+        return bz2.BZ2File(filename, 'w')
+else:
+    def bz2_open_write(filename):
+        return bz2.open(filename, 'wt')
+
+
+class Registry(Extension):
 
     def __init__(self, name):
         directory = os.path.join('registry', name)
         sources = glob.glob(os.path.join(directory, '*.gb'))
-        super(Registry, self).__init__(name, sources)
+        Extension.__init__(self, name, sources)
 
 
 class build_ext(_build_ext):
@@ -26,23 +34,22 @@ class build_ext(_build_ext):
         return os.path.join(*ext_name.split('.')) + '.json.bz2'
 
     def build_extension(self, ext):
-
+        # find directories
         registry = []
         gb_dir = os.path.dirname(ext.sources[0])
         dst_file = self.get_ext_fullpath(ext.name)
         dst_dir = os.path.dirname(dst_file)
-
+        # read all records
         self.announce('collecting records from {}'.format(gb_dir), 2)
         for gb_file in sorted(ext.sources):
             id_, _ = os.path.splitext(os.path.basename(gb_file))
             with open(gb_file) as gb_rec:
                 registry.append({'id': id_, 'gb': gb_rec.read()})
-
+        # write the compressed registry
         self.mkpath(dst_dir)
         self.announce('writing {} records to {}'.format(len(registry), dst_file), 2)
-        with bz2.open(dst_file, 'wt') as reg:
+        with bz2_open_write(dst_file) as reg:
             json.dump(registry, reg)
-
 
 
 if __name__ == '__main__':
