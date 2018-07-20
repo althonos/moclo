95bb3800f35575c0960ceeaf48aa8dd9728bd59d
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-07-20 15:30:02 +0200

    Improve annotations of `pPTK008` and `pPTK020`

--- scripts/ptk_registry.py
+++ scripts/ptk_registry.py
@@ -45,9 +45,11 @@ from moclo.regex import DNARegex
 URL = "https://www.addgene.org/kits/sieber-moclo-pichia-toolkit/#protocols-and-resources"
 UA = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
 
+# minimal Sequence from Bxb1 attB site from 10.1371/journal.pgen.1003490
+ATTB = DNARegex("GGCC(GGCTTGTCGACGACGGCGGTCNCCGTCGTCAGGATCAT)CCGG")
+ALPHA_MF_DELTA = None
 
 COLOR_REGEX = re.compile(r"color: (#[0-9a-fA-F]{6})")
-ALPHA_MF_DELTA = None
 
 
 def translate_color(feature):
@@ -63,7 +65,9 @@ def translate_color(feature):
         {
             "ApEinfo_fwdcolor": [hex_color],
             "ApEinfo_revcolor": [hex_color],
-            "ApEinfo_graphicformat": ["arrow_data {{0 1 2 0 0 -1} {} 0} width 5 offset 0"],
+            "ApEinfo_graphicformat": [
+                "arrow_data {{0 1 2 0 0 -1} {} 0} width 5 offset 0"
+            ],
         }
     )
 
@@ -102,7 +106,9 @@ if __name__ == "__main__":
         section = soup.find("section", id="addgene-full")
         gb_url = section.find("a", class_="genbank-file-download").get("href")
         with requests.get(gb_url) as res:
-            gbd = info["gb_depositor"] = CircularRecord(read(io.StringIO(res.text), "gb"))
+            gbd = info["gb_depositor"] = CircularRecord(
+                read(io.StringIO(res.text), "gb")
+            )
 
         # get the AddGene plasmid page
         url = "https://www.addgene.org/{}/".format(info["addgene_id"])
@@ -113,7 +119,9 @@ if __name__ == "__main__":
         section = soup.find("ul", class_="addgene-document-list")
         gb_url = section.find("a").get("href")
         with requests.get(gb_url) as res:
-            gba = info["gb_addgene"] = CircularRecord(read(io.StringIO(res.text), "gb"))
+            gba = info["gb_addgene"] = CircularRecord(
+                read(io.StringIO(res.text), "gb")
+            )
 
         # Sanity check
         if len(gba) != len(gbd):
@@ -141,7 +149,9 @@ if __name__ == "__main__":
         features = gba.features + gbd.features
 
         def get_features(label):
-            return (f for f in features if label in f.qualifiers.get("label", []))
+            return (
+                f for f in features if label in f.qualifiers.get("label", [])
+            )
 
         def get_features_from_note(note):
             return (f for f in features if note in f.qualifiers.get("note", []))
@@ -159,7 +169,10 @@ if __name__ == "__main__":
                     #          "2: 10 .. 13 / #ff0000\n"]
                 },
                 location=CompoundLocation(
-                    [FeatureLocation(2, 8, strand=1), FeatureLocation(9, 13, strand=1)]
+                    [
+                        FeatureLocation(2, 8, strand=1),
+                        FeatureLocation(9, 13, strand=1),
+                    ]
                 ),
             )
         )
@@ -192,7 +205,13 @@ if __name__ == "__main__":
         camr, cmr = next(get_features("CamR")), next(get_features("CmR"))
         features.remove(camr)
         cmr.type = "CDS"
-        cmr.qualifiers.update({k: v for k, v in camr.qualifiers.items() if k not in cmr.qualifiers})
+        cmr.qualifiers.update(
+            {
+                k: v
+                for k, v in camr.qualifiers.items()
+                if k not in cmr.qualifiers
+            }
+        )
         cmr.qualifiers.update(
             {
                 "codon_start": [1],
@@ -215,12 +234,18 @@ if __name__ == "__main__":
         cmr_prom = next(get_features("CamR Promoter"))
         cmr_prom.type = "promoter"
         cmr_prom.qualifiers.update(
-            {"label": ["CmR Promoter"], "note": ["color: #66ccff; direction: LEFT"]}
+            {
+                "label": ["CmR Promoter"],
+                "note": ["color: #66ccff; direction: LEFT"],
+            }
         )
         cmr_term = next(get_features("CamR Terminator"))
         cmr_term.type = "terminator"
         cmr_term.qualifiers.update(
-            {"label": ["CmR Terminator"], "note": ["color: #66ccff; direction: LEFT"]}
+            {
+                "label": ["CmR Terminator"],
+                "note": ["color: #66ccff; direction: LEFT"],
+            }
         )
 
         # Make sure ColE1 is grayed
@@ -230,7 +255,11 @@ if __name__ == "__main__":
 
         # plasmid-specific annotations
         if id_ == "pPTK001":
-            p1, p2 = [f for f in features if any("AOX1" in l for l in f.qualifiers.get("gene", []))]
+            p1, p2 = [
+                f
+                for f in features
+                if any("AOX1" in l for l in f.qualifiers.get("gene", []))
+            ]
             features.remove(p2)
             p1.type = "promoter"
             p1.qualifiers.update(
@@ -261,6 +290,13 @@ if __name__ == "__main__":
                     ],
                 }
             )
+
+            mod = next(get_features_from_note("Modification for no digestion"))
+            mod.type = "misc_difference"
+            mod.qualifiers = {
+                "replace": ["c"],
+                "note": ["remove BsmBI cutting site"],
+            }
 
         elif id_ == "pPTK003":
             pENO = next(get_features_from_note("pENO"))
@@ -340,9 +376,12 @@ if __name__ == "__main__":
                     # 'direction': ['RIGHT'],
                     "label": ["MF-alpha-1 signal (no EAEA)"],
                     "gene": ["S. cerevisiae MF(ALPHA)1"],
-                    "product": ["mating factor alpha-1 secretion signal, no EAEA"],
+                    "product": [
+                        "mating factor alpha-1 secretion signal, no EAEA"
+                    ],
                     "note": [
-                        "Cleavage by the Kex2 protease occurs after the " "dibasic KR sequence.",
+                        "Cleavage by the Kex2 protease occurs after the "
+                        "dibasic KR sequence.",
                         "color: #ffcbbf; direction: RIGHT",
                     ],
                     "db_xref": [
@@ -364,7 +403,9 @@ if __name__ == "__main__":
                     # 'direction': ['RIGHT'],
                     "label": ["MF-alpha-1-delta signal"],
                     "gene": ["S. cerevisiae MF(ALPHA)1"],
-                    "product": ["mating factor alpha-1 secretion signal, shortened"],
+                    "product": [
+                        "mating factor alpha-1 secretion signal, shortened"
+                    ],
                     "note": ["color: #ffcbbf; direction: RIGHT"],
                     "db_xref": [
                         "UniProtKB/Swiss-Prot:P01149",
@@ -374,12 +415,63 @@ if __name__ == "__main__":
                     ],
                 }
             )
-            ALPHA_MF_DELTA = DNARegex(str(ad.extract(gba.seq)).replace("GCCGCTA", "GCC[GT]CTA"))
+            ALPHA_MF_DELTA = DNARegex(
+                str(ad.extract(gba.seq)).replace("GCCGCTA", "GCC[GT]CTA")
+            )
+
+        elif id_ == "pPTK008":
+            adk = next(get_features_from_note("AlphaT*"))
+            adk.type = "sig_peptide"
+            adk.location = FeatureLocation(
+                adk.location.start, adk.location.start + 57, 1
+            )
+            adk.qualifiers = {
+                "label": ["MF-alpha-1 signal pre-sequence"],
+                "gene": ["S. cerevisiae MF(ALPHA1)"],
+                "product": [
+                    "mating factor alpha-1 secretion signal pre-sequence"
+                ],
+                "note": ["color: #ff6600; direction: RIGHT"],
+                "db_xref": [
+                    "UniProtKB/Swiss-Prot:P01149",
+                    "InterPro:IPR008675",
+                    "PFAM:PF05436",
+                    "SGD:S000006108",
+                ],
+            }
+
+            moda = next(get_features_from_note("AlphaT"))
+            moda.type = "misc_difference"
+            moda.qualifiers = {"replace": ["G"]}
+
+            ank = next(get_features_from_note("AlphaT 3"))
+            ank.type = "sig_peptide"
+            ank.location = FeatureLocation(
+                adk.location.start + 58, ank.location.end, 1
+            )
+            ank.qualifiers = {
+                "codon_start": ["1"],
+                # 'direction': ['RIGHT'],
+                "label": ["alpha-MF-delta (no Kex)"],
+                "gene": ["S. cerevisiae MF(ALPHA)1"],
+                "product": [
+                    "mating factor alpha-1 secretion signal, shortened, no Kex2p site"
+                ],
+                "note": ["color: #ffcbbf; direction: RIGHT"],
+                "db_xref": [
+                    "UniProtKB/Swiss-Prot:P01149",
+                    "InterPro:IPR008675",
+                    "PFAM:PF05436",
+                    "SGD:S000006108",
+                ],
+            }
 
         elif id_ == "pPTK009":
             am = next(get_features_from_note("alpha-amylase"))
             am.type = "sig_peptide"
-            am.location = FeatureLocation(am.location.start, am.location.start + 60, +1)
+            am.location = FeatureLocation(
+                am.location.start, am.location.start + 60, +1
+            )
             am.qualifiers = {
                 "gene": ["amy"],
                 "product": ["alpha-amylase signal"],
@@ -391,7 +483,9 @@ if __name__ == "__main__":
         elif id_ == "pPTK010":
             gam = next(get_features_from_note("Glucoamylase"))
             gam.type = "sig_peptide"
-            gam.location = FeatureLocation(gam.location.start, gam.location.start + 54, +1)
+            gam.location = FeatureLocation(
+                gam.location.start, gam.location.start + 54, +1
+            )
             gam.qualifiers = {
                 "gene": ["gaI"],
                 "product": ["glucoamylase I signal"],
@@ -403,7 +497,9 @@ if __name__ == "__main__":
         elif id_ == "pPTK011":
             alb = next(get_features_from_note("hSA"))
             alb.type = "sig_peptide"
-            alb.location = FeatureLocation(alb.location.start, alb.location.start + 54, +1)
+            alb.location = FeatureLocation(
+                alb.location.start, alb.location.start + 54, +1
+            )
             alb.qualifiers = {
                 "gene": ["ALB"],
                 "product": ["serum albumin signal"],
@@ -415,7 +511,9 @@ if __name__ == "__main__":
         elif id_ == "pPTK012":
             inu = next(get_features_from_note("Inulinase"))
             inu.type = "sig_peptide"
-            inu.location = FeatureLocation(inu.location.start, inu.location.start + 48, +1)
+            inu.location = FeatureLocation(
+                inu.location.start, inu.location.start + 48, +1
+            )
             inu.qualifiers = {
                 "gene": ["INU1"],
                 "product": ["inulinase signal peptide"],
@@ -427,7 +525,9 @@ if __name__ == "__main__":
         elif id_ == "pPTK013":
             inv = next(get_features_from_note("Invertase"))
             inv.type = "sig_peptide"
-            inv.location = FeatureLocation(inv.location.start, inv.location.start + 57, +1)
+            inv.location = FeatureLocation(
+                inv.location.start, inv.location.start + 57, +1
+            )
             inv.qualifiers = {
                 "gene": ["SUC1"],
                 "product": ["invertase 1 signal peptide"],
@@ -439,12 +539,41 @@ if __name__ == "__main__":
         elif id_ == "pPTK014":
             kil = next(get_features_from_note("Killer"))
             kil.type = "sig_peptide"
-            kil.location = FeatureLocation(kil.location.start, kil.location.start + 78, 1)
+            kil.location = FeatureLocation(
+                kil.location.start, kil.location.start + 78, 1
+            )
             kil.qualifiers = {
                 "product": ["M1-1 killer toxin signal peptide"],
                 "note": ["color: #ff6600; direction: RIGHT"],
                 "label": ["Killer toxin signal"],
                 "db_xref": ["UniProtKB/Swiss-Prot:P01546"],
+            }
+
+        elif id_ == "pPTK020":
+            attb1 = next(get_features_from_note("BxbI attB"))
+            attb2 = next(get_features_from_note("attB"))
+            features.remove(attb1)
+            start, end = ATTB.search(gba.seq).span(1)
+            attb2.type = "protein_bind"
+            attb2.location = FeatureLocation(start, end, 1)
+            attb2.qualifiers = {
+                "label": ["attB"],
+                "bound_moiety": ["Bxb1 integrase"],
+                "function": ["Bxb1 integrase attB recognition site"],
+                "note": ["color: #99cc00"],
+            }
+
+            uas = next(get_features_from_note("UASrpg"))
+            uas.type = "misc_feature"
+            uas.qualifiers = {
+                "label": ["TKL2 3' Homology"],
+                "function": [
+                    "upstream activation site for ribosomal protein genes"
+                ],
+                "note": [
+                    "color: #6685ca",
+                    "TEF2 UASrpg from S. cerevisiae chromosome II",
+                ],
             }
 
         egfp2 = next(get_features("EGFP"), None)
@@ -455,26 +584,29 @@ if __name__ == "__main__":
                 {
                     "codon_start": 1,
                     "label": ["eGFP"],
-                    "gene": ["GFP"]
+                    "gene": ["GFP"],
                     "product": ["enhanced green fluorescent protein"],
-                    "note": ["mammalian codon-optimized", "color: #34FF03; direction: RIGHT"],
+                    "note": [
+                        "mammalian codon-optimized",
+                        "color: #34FF03; direction: RIGHT",
+                    ],
                     "db_xref": [
-                        'UniProtKB/Swiss-Prot:P42212',
-                        'PDB:1EMA',
-                        'InterPro:IPR009017',
-                        'InterPro:IPR011584',
-                        'InterPro:IPR000786',
-                        'PFAM:PF01353',
-                        'GO:0008218',
-                        'GO:0018298',
-                    ]
+                        "UniProtKB/Swiss-Prot:P42212",
+                        "PDB:1EMA",
+                        "InterPro:IPR009017",
+                        "InterPro:IPR011584",
+                        "InterPro:IPR000786",
+                        "PFAM:PF01353",
+                        "GO:0008218",
+                        "GO:0018298",
+                    ],
                 }
             )
 
         rfp = next(get_features_from_note("RFP"), None)
         if rfp is not None:
             rfp.type = "CDS"
-            start = rfp.location.start + rfp.extract(gba.seq).find('ATG', 1)
+            start = rfp.location.start + rfp.extract(gba.seq).find("ATG", 1)
             rfp.location = FeatureLocation(start, start + 708, 1)
             rfp.qualifiers.update(
                 {
@@ -482,7 +614,9 @@ if __name__ == "__main__":
                     "label": ["RFP"],
                     "gene": ["mCherry"],
                     "product": ["mCherry"],
-                    "translation": str(translate(rfp.extract(gba.seq), to_stop=True)),
+                    "translation": str(
+                        translate(rfp.extract(gba.seq), to_stop=True)
+                    ),
                     "note": ["color: #c16969; direction: RIGHT"],
                     "db_xref": [
                         "UniProtKB/Swiss-Prot:X5DSL3",
@@ -497,7 +631,11 @@ if __name__ == "__main__":
                 }
             )
 
-            alert = next(get_features_from_note("Start with second codon!!! ATG deleted!"))
+            alert = next(
+                get_features_from_note(
+                    "Start with second codon!!! ATG deleted!"
+                )
+            )
             features.remove(alert)
 
         pars = next(get_features_from_note("PARS-1"), None)
@@ -512,7 +650,10 @@ if __name__ == "__main__":
             features.remove(t2)
             t1.qualifiers.update(
                 {
-                    "note": ["transcription terminator for AOX1", "color: #ff8eff"],
+                    "note": [
+                        "transcription terminator for AOX1",
+                        "color: #ff8eff",
+                    ],
                     "label": ["PpAOX1 Terminator"],
                 }
             )
@@ -536,7 +677,8 @@ if __name__ == "__main__":
 
         # sort features
         features.sort(
-            key=lambda feat: (-len(gba.seq)) * (feat.type == "source") + feat.location.start
+            key=lambda feat: (-len(gba.seq)) * (feat.type == "source")
+            + feat.location.start
         )
 
         # translate color from notes to ApEinfo
