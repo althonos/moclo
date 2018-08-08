d536b9251b99d585677660120193fad84e8e53e3
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-08-08 19:17:33 +0200

    Add implementation of the Icon Genetics MoClo toolkit

---
+++ moclo-ig/moclo/kits/ig.py
@@ -0,0 +1,240 @@
+# coding: utf-8
+"""An implementation of the Icon Genetics ToolKit for the Python MoClo library.
+
+References:
+    1. `Weber, E., Engler, C., Gruetzner, R., Werner, S., Marillonnet, S. (2011).
+       A Modular Cloning System for Standardized Assembly of Multigene Constructs.
+       PLOS ONE, 6(2), e16765.
+       <https://doi.org/10.1371/journal.pone.0016765>`_
+    2. `Werner, S., Engler, C., Weber, E., Gruetzner, R., & Marillonnet, S. (2012).
+       Fast track assembly of multigene constructs using Golden Gate cloning
+       and the MoClo system. Bioengineered, 3(1), 38–43.
+       <https://doi.org/10.4161/bbug.3.1.18223>`_
+"""
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from Bio.Restriction import BsaI, BpiI
+
+from ..core import parts, modules, vectors
+
+__author__ = 'Martin Larralde <martin.larralde@ens-paris-saclay.fr>'
+__version__ = (
+    __import__('pkg_resources')
+        .resource_string(__name__, 'ig.version')
+        .strip()
+        .decode('ascii')
+)
+
+
+### Vectors
+
+class IGEntryVector(vectors.EntryVector):
+    """An Icon Genetics entry vector.
+
+    References:
+        *Weber et al.*, Figure 2A.
+
+    """
+
+    cutter = BpiI
+
+    @classmethod
+    def structure(cls):
+        return (
+            'GGTCTC'  # BsaI
+            'N'
+            '(NNNN)'  # Downstream overhang
+            '(NN'
+            'GTCTTC'  # BpiI
+            'N*'      # Placeholder sequence
+            'GAAGAC'  # BpiI
+            'NN)'
+            '(NNNN)'  # Upstream overhang
+            'N'
+            'GAGACC'  # BsaI
+    )
+
+
+class IGCassetteVector(vectors.CassetteVector):
+    """An Icon Genetics cassette vector.
+
+    References:
+        *Weber et al.*, Figure 4A.
+
+    """
+
+    cutter = BsaI
+
+    @classmethod
+    def structure(cls):  # noqa: D105
+        return (
+            'GAAGAC'  # BpiI
+            'NN'
+            'NNNN'    # Cassette upstream overhang
+            '(NNNN)'  # Cassette vector downstream overhang
+            '(N'
+            'GAGACC'  # BsaI
+            'N*'      # Placeholder sequence
+            'GGTCTC'  # BsaI
+            'N)'
+            '(NNNN)'  # Cassette vector upstream overhang
+            'NNNN'    # Cassette downstream overhang
+            'NN'
+            'GTCTTC'  # BbsI
+        )
+
+
+class IGSingleCassetteVector(vectors.CassetteVector):
+    """An Icon Genetics single cassette vector.
+
+    These plasmids allow obtaining plasmids ready for plant transformation
+    containing only a single transcription unit, assembled directly from
+    entries, without using an intermediate *E. coli* plasmid.
+
+    Danger:
+        The resulting plasmid will not be a valid `IGCassette`!
+
+    References:
+        `AddGene MoClo <https://www.addgene.org/cloning/moclo/marillonnet/>`_
+        construct reference.
+
+    """
+
+    cutter = BsaI
+
+
+class IGDeviceVector(vectors.DeviceVector):
+    """An Icon Genetics device vector.
+
+    References:
+        *Weber et al.*, Figure 4A.
+
+    """
+
+    cutter = BpiI
+
+
+### Modules
+
+class IGProduct(modules.Product):
+    """An Icon Genetics MoClo product.
+    """
+
+    cutter = BpiI
+
+
+class IGEntry(modules.Entry):
+    """An Icon Genetics MoClo entry.
+    """
+
+    cutter = BsaI
+
+
+class IGCassette(modules.Cassette):
+    """An Icon Genetics MoClo cassette.
+    """
+
+    cutter = BpiI
+
+
+### Parts
+
+class IGPart(parts.AbstractPart):
+    """An Icon Genetics MoClo standard part.
+
+    A part is a plasmid with standardized flanking overhang sequences
+    that allows immediate type recognition.
+    """
+
+    cutter = BsaI
+    signature = NotImplemented
+
+
+class IGPromoter(IGPart, IGEntry):
+    """An Icon Genetics promoter part.
+    """
+
+    signature = ('GGAG', 'TACT')
+
+
+class IGUntranslatedRegion(IGPart, IGEntry):
+    """An Icon Genetics 5' UTR part.
+    """
+
+    signature = ('TACT', 'AATG')
+
+
+class IGSignalPeptide(IGPart, IGEntry):
+    """An Icon Genetics signal peptide part.
+    """
+
+    signature = ('AATG', 'AGGT')
+
+
+class IGCodingSequence(IGPart, IGEntry):
+    """An Icon Genetics CDS part.
+    """
+
+    signature = ('AGGT', 'GCTT')
+
+
+class IGTerminator(IGPart, IGEntry):
+    """An Icon Genetics terminator part.
+    """
+
+    signature = ('GCTT', 'CGCT')
+
+
+class IGEndLinker(IGPart, IGCassette):
+    """An Icon Genetic end linker part.
+
+    References:
+        *Weber et al.*, Figure 5.
+
+    """
+
+    signature = ('NNNN', 'GGGA')
+
+
+class IGLevelMEndLinker(IGPart, IGCassette):
+
+    signature = ('NNNN', 'GGGA')
+
+    # FIXME: add prefix BsaI
+    @classmethod
+    def structure(cls):
+        return super(IGLevelMEndLinker, cls).structure()
+
+
+class IGLevelMVector(IGPart, IGDeviceVector):
+
+    cutter = BpiI
+    signature = ('GGGA', 'NNNN')
+
+    # FIXME: add prefix BsaI
+    @classmethod
+    def structure(cls):
+        return super(IGLevelMVector, cls).structure()
+
+
+class IGLevelPEndLinker(IGPart, IGEntry):  # FIXME: hierarchy ?
+
+    cutter = BsaI
+    signature = ('NNNN', 'GGGA')
+
+    # FIXME: add prefix BpiI
+    @classmethod
+    def structure(cls):
+        return super(IGLevelPEndLinker, cls).structure()
+
+
+class IGLevelPVector(IGPart, IGCassetteVector):  # FIXME: hierarchy ?
+
+    cutter = BsaI
+    signature = ('GGGA', 'NNNN')
+
+    # FIXME: add prefix BpiI
+    @classmethod
+    def structure(cls):
+        return super(IGLevelPVector, cls).structure()
---
+++ moclo-ig/moclo/registry/ig.py
@@ -0,0 +1,105 @@
+# coding: utf-8
+"""Icon Genetics ToolKit sequences registry.
+"""
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import re
+
+import six
+
+from ..kits import ig
+from .base import EmbeddedRegistry
+from ._utils import find_resistance
+
+
+class IGRegistry(EmbeddedRegistry):
+
+    _module = __name__
+    _file = "ig.json.bz2"
+
+    def _load_name(self, raw, index):
+        return raw['record'].name
+
+    def _load_id(self, raw, index):
+        return raw['record'].id
+
+    def _load_resistance(self, raw, index):
+        # TODO: sanitize sequences in `moclo-ig/registry/ig` to use the
+        #       right sequence labels instead of faulty ones
+        #
+        # try:
+        #     return find_resistance(raw['record'])
+        # except RuntimeError:
+        #     msg = "could not find antibiotics resistance of '{}'"
+        #     six.raise_from(RuntimeError(msg.format(raw['record'].id)), None)
+        # return raw['resistance']
+        #
+        labels =  [label for f in raw['record'].features
+                         for label in f.qualifiers.get('label', [])]
+        if 'AP r' in labels or 'AP\\r' in labels or 'AP(R)' in labels:
+            return 'Ampicillin'
+        elif 'Sm/Sp\\no\\DraIII' in labels or 'Sm/Sp' in labels or 'spec' in labels or 'spec\orf?' in labels:
+            return 'Spectinomycin'
+        elif 'NPTII' in labels or 'Kan\(no\BpiI)' in labels:
+            return 'Kanamycin'
+        else:
+            raise RuntimeError('antibio of ' + raw['record'].name)
+
+
+    # _ENTITY_RX = re.compile(r'MoClo (.*): ([^\-\[\(]*)')
+    #
+    # _CLASSES = {
+    #     'Cassette Vector': cidar.CIDARCassetteVector,
+    #     'Entry Vector': cidar.CIDAREntryVector,
+    #     'Device': cidar.CIDARDevice,
+    #     'Transcriptional Unit': cidar.CIDARCassette,
+    #     'Basic Part': cidar.CIDARPart,
+    # }
+    #
+    # _TYPES = {
+    #     'Double terminator': cidar.CIDARTerminator,
+    #     'RBS': cidar.CIDARRibosomeBindingSite,
+    #     'CDS': cidar.CIDARCodingSequence,
+    #     'Controllable promoter': cidar.CIDARPromoter,
+    #     'Constitutive promoter': cidar.CIDARPromoter,
+    # }
+
+    def _load_entity(self, raw, index):
+
+        types = [
+            ig.IGLevelPEndLinker,
+            ig.IGLevelMEndLinker,
+            ig.IGLevelPVector,
+            ig.IGLevelMVector,
+            ig.IGEndLinker,
+            ig.IGTerminator,
+            ig.IGCodingSequence,
+            ig.IGSignalPeptide,
+            ig.IGUntranslatedRegion,
+            ig.IGPromoter,
+            ig.IGCassette,
+            ig.IGCassetteVector,
+            ig.IGSingleCassetteVector,
+            ig.IGDeviceVector,
+        ]
+
+        for t in types:
+            entity = t(raw['record'])
+            if entity.is_valid():
+                return entity
+        raise RuntimeError('entity of ' + raw['record'].name)
+
+        # match = self._ENTITY_RX.match(raw['record'].description)
+        # if match is not None:
+        #     class_, type_ = map(six.text_type.strip, match.groups())
+        #     if class_ == 'Destination Vector':
+        #         if raw['id'].startswith('DVA'):
+        #             class_ = 'Entry Vector'
+        #         elif raw['id'].startswith('DVK'):
+        #             class_ = 'Cassette Vector'
+        #     if class_ != 'Basic Part':
+        #         return self._CLASSES[class_](raw['record'])
+        #     if type_ in self._TYPES:
+        #         return self._TYPES[type_](raw['record'])
+        # raise RuntimeError("could not find type of '{}'".format(raw['id']))
