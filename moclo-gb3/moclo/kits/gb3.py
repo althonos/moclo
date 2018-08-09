4257df3a8034b433317513b260290cba477371b5
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-08-09 17:54:30 +0200

    Improve documentation of `moclo-gb3`

--- moclo-gb3/moclo/kits/gb3.py
+++ moclo-gb3/moclo/kits/gb3.py
@@ -8,16 +8,17 @@ from Bio.Restriction import BsaI, BsmBI
 
 from ..core import parts, modules, vectors
 
-__author__ = 'Martin Larralde <martin.larralde@ens-paris-saclay.fr>'
+__author__ = "Martin Larralde <martin.larralde@ens-paris-saclay.fr>"
 __version__ = (
-    __import__('pkg_resources')
-        .resource_string(__name__, 'gb3.version')
-        .strip()
-        .decode('ascii')
+    __import__("pkg_resources")
+    .resource_string(__name__, "gb3.version")
+    .strip()
+    .decode("ascii")
 )
 
 
 # VECTORS ####################################################################
+
 
 class GB3OmegaVector(vectors.CassetteVector):
     """A GoldenBraid 3.0 Level Omega vector.
@@ -28,17 +29,17 @@ class GB3OmegaVector(vectors.CassetteVector):
     @classmethod
     def structure(cls):  # noqa: D105
         return (
-            'CGTCTC'  # BsmBI
-            'NN'
-            '(NNNN)'  # Vector downstream overhang
-            '(N'
-            'GAGACC'  # BsaI
-            'N*'      # Placeholder sequence
-            'GGTCTC'  # BsaI
-            'N)'
-            '(NNNN)'  # Vector upstream overhang
-            'NN'
-            'GAGACG'  # BsmBI
+            "CGTCTC"  # BsmBI
+            "NN"
+            "(NNNN)"  # Vector downstream overhang
+            "(N"
+            "GAGACC"  # BsaI
+            "N*"  # Placeholder sequence
+            "GGTCTC"  # BsaI
+            "N)"
+            "(NNNN)"  # Vector upstream overhang
+            "NN"
+            "GAGACG"  # BsmBI
         )
 
 
@@ -51,21 +52,22 @@ class GB3AlphaVector(vectors.DeviceVector):
     @classmethod
     def structure(cls):  # noqa: D105
         return (
-            'GGTCTC'  # BsaI
-            'N'
-            '(NNNN)'  # Vector downstream overhang
-            '(N'
-            'GAGACG'  # BsmBI
-            'N*'      # Placeholder sequence
-            'CGTCTC'  # BsmBI
-            'N)'
-            '(NNNN)'  # Vector upstream overhang
-            'N'
-            'GAGACC'  # BsaI
+            "GGTCTC"  # BsaI
+            "N"
+            "(NNNN)"  # Vector downstream overhang
+            "(N"
+            "GAGACG"  # BsmBI
+            "N*"  # Placeholder sequence
+            "CGTCTC"  # BsmBI
+            "N)"
+            "(NNNN)"  # Vector upstream overhang
+            "N"
+            "GAGACC"  # BsaI
         )
 
 
 # MODULES ####################################################################
+
 
 class GB3OmegaModule(modules.Entry):
     """A GoldenBraid 3.0 Level Omega module.
@@ -73,13 +75,16 @@ class GB3OmegaModule(modules.Entry):
 
     cutter = BsaI
 
+
 class GB3AlphaModule(modules.Cassette):
     """A GoldenBraid 3.0 Level Alpha module.
     """
 
     cutter = BsmBI
 
+
 # PARTS ######################################################################
+
 
 class GB3Part(parts.AbstractPart):
     """An GoldenBraid 3.0 part, also refered as *element* in the GB lexicon.
@@ -91,56 +96,108 @@ class GB3Part(parts.AbstractPart):
     cutter = BsaI
     signature = NotImplemented
 
+
 # Transcriptional Units Parts ################################################
 
-class GB3Dist(GB3Part, GB3OmegaModule):
-    signature = ('GGAG', 'TGAC')
 
-class GB3Prox(GB3Part, GB3OmegaModule):
-    signature = ('TGAC', 'TCCC')
+class GB3DistalOperator(GB3Part, GB3OmegaModule):
+    """A GoldenBraid 3.0 Distal Operator.
+    """
 
-class GB3Core(GB3Part, GB3OmegaModule):
-    signature = ('TCCC', 'TACT')
+    signature = ("GGAG", "TGAC")
+
+
+class GB3ProximalOperator(GB3Part, GB3OmegaModule):
+    """A GoldenBraid 3.0 Proximal Operator.
+    """
+
+    signature = ("TGAC", "TCCC")
+
+
+class GB3CorePromoter(GB3Part, GB3OmegaModule):
+    """A GoldenBraid 3.0 Core Promoter.
+    """
+
+    signature = ("TCCC", "TACT")
+
 
 class GB3UpstreamUTR(GB3Part, GB3OmegaModule):
-    signature = ('TACT', 'CCAT')
+    """A GoldenBraid 3.0 5' Untranslated Region.
+    """
+
+    signature = ("TACT", "CCAT")
+
 
 class GB3NTag(GB3Part, GB3OmegaModule):
-    signature = ('CCAT', 'AATG')
+    """A GoldenBraid 3.0 N-terminal protein tag.
+    """
+
+    signature = ("CCAT", "AATG")
+
 
 class GB3CodingSequence1(GB3Part, GB3OmegaModule):
-    signature = ('AATG', 'AGCC')
+    signature = ("AATG", "AGCC")
+
 
 class GB3CodingSequence2(GB3Part, GB3OmegaModule):
-    signature = ('AGCC', 'TTCG')
+    signature = ("AGCC", "TTCG")
+
 
 class GB3CTag(GB3Part, GB3OmegaModule):
-    signature = ('TTCG', 'GCTT')
+    """A GoldenBraid 3.0 C-terminal protein tag.
+    """
+
+    signature = ("TTCG", "GCTT")
+
 
 class GB3DownstreamUTR(GB3Part, GB3OmegaModule):
-    signature = ('GCTT', 'GGTA')
+    """A GoldenBraid 3.0 3' Untranslated Region.
+    """
+
+    signature = ("GCTT", "GGTA")
+
 
 class GB3Terminator(GB3Part, GB3OmegaModule):
-    signature = ('GGTA', 'CGCT')
+    """A GoldenBraid 3.0 Terminator.
+    """
+
+    signature = ("GGTA", "CGCT")
+
 
 # Crisprs Parts ##############################################################
 
+
 class GB3PolIIIMonocotPromoter(GB3Part, GB3OmegaModule):
-    signature = ('GGAG', 'GGCA')
+    signature = ("GGAG", "GGCA")
+
 
 class GB3MTarget(GB3Part, GB3OmegaModule):
-    signature = ('GGCA', 'GTTT')
+    signature = ("GGCA", "GTTT")
+
 
 class GB3PolIIIDicotPromoter(GB3Part, GB3OmegaModule):
-    signature = ('GGAG', 'ATTG')
+    signature = ("GGAG", "ATTG")
+
 
 class GB3DTarget(GB3Part, GB3OmegaModule):
-    signature = ('ATTG', 'GTTT')
+    signature = ("ATTG", "GTTT")
+
 
 class GB3sgRNA(GB3Part, GB3OmegaModule):
-    signature = ('GTTT', 'CGCT')
+    """A GoldenBraid 3.0 sgRNA coding sequence.
+    """
+
+    signature = ("GTTT", "CGCT")
+
 
 # Composite Parts ############################################################
 
+
 class GB3InteractionAdaptor(GB3Part, GP3OmegaModule):
-    signature = ('GGAG', 'AATG')
+    """A GoldenBraid 3.0 Interaction Adaptor.
+
+    An Interaction Adaptor is made of a complete promoter (distal and proximal
+    operators, and a core promoter) followed by a 5'UTR and a N-terminal tag.
+    """
+
+    signature = ("GGAG", "AATG")
