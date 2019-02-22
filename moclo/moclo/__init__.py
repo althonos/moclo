28a03748df8a2fa43f0c0c8098ca64d11559434e
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-02-22 13:42:25 +0100

    Release v0.4.5

--- moclo/moclo/__init__.py
+++ moclo/moclo/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import as _
 from __future__ import unicode_literals as _
 
 __author__ = "Martin Larralde <martin.larralde@ens-paris-saclay.fr>"
-__version__ = "0.4.4"
+__version__ = "0.4.5"
