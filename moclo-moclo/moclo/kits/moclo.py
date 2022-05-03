7d2e177654d6dac3d8fe40e4345835660bec155d
Martin Larralde <martin.larralde@embl.de>
2022-05-03 15:27:44 +0200

    Add new parts from 2020 MoClo review to the `moclo-moclo` kit

--- moclo-moclo/moclo/kits/moclo.py
+++ moclo-moclo/moclo/kits/moclo.py
@@ -1,15 +1,22 @@
 # coding: utf-8
 """An implementation of the original MoClo ToolKit for the Python MoClo library.
 
+.. image:: https://media.springernature.com/original/springer-static/image/chp%3A10.1007%2F978-1-0716-0908-8_8/MediaObjects/468902_1_En_8_Fig2_HTML.png
+   :target: https://link.springer.com/protocol/10.1007/978-1-0716-0908-8_8
+
 References:
     1. `Weber, E., Engler, C., Gruetzner, R., Werner, S., Marillonnet, S. (2011).
        A Modular Cloning System for Standardized Assembly of Multigene Constructs.
        PLOS ONE, 6(2), e16765.
-       <https://doi.org/10.1371/journal.pone.0016765>`_
+       <https://doi.org/10.1371/journal.pone.0016765>`_.
     2. `Werner, S., Engler, C., Weber, E., Gruetzner, R., & Marillonnet, S. (2012).
        Fast track assembly of multigene constructs using Golden Gate cloning
        and the MoClo system. Bioengineered, 3(1), 38–43.
-       <https://doi.org/10.4161/bbug.3.1.18223>`_
+       <https://doi.org/10.4161/bbug.3.1.18223>`_.
+    3. `Marillonnet, S., Werner, S. (2020).
+       Assembly of Multigene Constructs Using the Modular Cloning System
+       MoClo. Methods Mol Biol. 2020;2205:125-141.
+       <https://10.1007/978-1-0716-0908-8_8>`_.
 
 """
 from __future__ import absolute_import
@@ -160,45 +167,108 @@ class MoCloPart(parts.AbstractPart):
 # Level 0 ####################################################################
 
 
-class MoCloPromoter(MoCloPart, MoCloEntry):
+class MoCloPro(MoCloPart, MoCloEntry):
     """An original MoClo promoter part.
     """
 
     signature = ("GGAG", "TACT")
 
 
-class MoCloUntranslatedRegion(MoCloPart, MoCloEntry):
+class MoClo5U(MoCloPart, MoCloEntry):
     """An original MoClo 5' UTR part.
     """
 
     signature = ("TACT", "AATG")
 
 
-class MoCloSignalPeptide(MoCloPart, MoCloEntry):
+class MoClo5Uf(MoCloPart, MoCloEntry):
+    """An original MoClo 5'UTR part for N-terminal tag linkage.
+    """
+
+    signature = ("TACT", "CCAT")
+
+
+class MoCloNTag(MoCloPart, MoCloEntry):
+    """An original MoClo N-terminal tag part.
+    """
+
+    signature = ("CCAT", "AATG")
+
+
+class MoCloPro5U(MoCloPart, MoCloEntry):
+    """An original MoClo promoter fused with a 5'UTR part.
+    """
+
+    signature = ("GGAG", "AATG")
+
+
+class MoCloPro5Uf(MoCloPart, MoCloEntry):
+    """An original MoClo promoter fused with a 5'UTR for N-terminal linkage.
+    """
+
+    signature = ("GGAG", "CCAT")
+
+
+class MoCloCDS1(MoCloPart, MoCloEntry):
+    """An original MoClo CDS1.
+    """
+
+    signature = ("AATG", "GCTT")
+
+
+class MoCloCDS1ns(MoCloPart, MoCloEntry):
+    """An original MoClo CDS1 without STOP codon for C-terminal tag linkage.
+    """
+
+    signature = ("AATG", "TTCG")
+
+
+class MoCloSP(MoCloPart, MoCloEntry):
     """An original MoClo signal peptide part."""
 
     signature = ("AATG", "AGGT")
 
 
-class MoCloCodingSequence(MoCloPart, MoCloEntry):
-    """An original MoClo CDS part.
+class MoCloCDS2(MoCloPart, MoCloEntry):
+    """An original MoClo CDS2 part.
     """
 
     signature = ("AGGT", "GCTT")
 
 
-class MoCloTerminator(MoCloPart, MoCloEntry):
+class MoCloCDS2ns(MoCloPart, MoCloEntry):
+    """An original MoClo CDS2 for C-terminal tag linkage.
+    """
+
+    signature = ("AGGT", "TTCG")
+
+
+class MoCloCTag(MoCloPart, MoCloEntry):
+    """An original MoClo C-terminal tag part.
+    """
+
+    signature = ("TTCG", "GCTT")
+
+
+class MoClo3U(MoCloPart, MoCloEntry):
+    """An original MoClo 3'UTR part.
+    """
+
+    signature = ("GCTT", "GGTA")
+
+
+class MoCloTer(MoCloPart, MoCloEntry):
+    """An original MoClo terminator part.
+    """
+
+    signature = ("GGTA", "CGCT")
+
+
+class MoClo3UTer(MoCloPart, MoCloEntry):
     """An original MoClo terminator part.
     """
 
     signature = ("GCTT", "CGCT")
-
-
-class MoCloGene(MoCloPart, MoCloEntry):
-    """An original MoClo part composed of an entire gene.
-    """
-
-    signature = ("GGAG", "CGCT")
 
 
 # Level 0 ####################################################################
